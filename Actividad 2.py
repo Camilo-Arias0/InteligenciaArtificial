cb66f09f70a9c3d41891667d8f883544e82c880a
AndresPardo09 <pipepardo9@gmail.com>
2026-03-27 22:00:22 -0500

    Update Actividad 2.py

--- Actividad 2.py
+++ Actividad 2.py
@@ -7,4 +7,23 @@ grafo["A"] = [("B", 5, "L1"), ("D", 10, "L2")]
 grafo["B"] = [("C", 7, "L1")]
 grafo["D"] = [("C", 3, "L2")]
 
+# Reglas (base de conocimiento)
+def evaluar(costo, cambio_linea):
+    return costo + (5 if cambio_linea else 0)
 
+# A* simplificado
+def buscar(inicio, fin):
+    cola = [(0, inicio, None, [])]
+    visitados = set()
+
+    while cola:
+        costo, nodo, linea, ruta = heapq.heappop(cola)
+
+        if nodo in visitados:
+            continue
+
+        ruta = ruta + [nodo]
+        visitados.add(nodo)
+
+        if nodo == fin:
+            return ruta, costo
