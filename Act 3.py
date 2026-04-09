ec328dff60bcd1f6a6c73d9641e4b5a75e9b1f6e
David Ramos <109716289+craken010@users.noreply.github.com>
2026-04-08 22:34:05 -0500

    entrenamiento de modelo

--- Act 3.py
+++ Act 3.py
@@ -30,4 +30,27 @@ y = df["mejor_ruta"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
+# ==============================
+# 4. ENTRENAR MODELO
+# ==============================
 
+modelo = DecisionTreeClassifier()
+modelo.fit(X_train, y_train)
+
+# ==============================
+# 5. EVALUAR
+# ==============================
+
+y_pred = modelo.predict(X_test)
+print("Precisión del modelo:", accuracy_score(y_test, y_pred))
+
+# ==============================
+# 6. PREDICCIÓN
+# ==============================
+
+nueva_ruta = pd.DataFrame([[9, 1, 0]],
+                          columns=["tiempo", "transbordos", "hora_pico"])
+
+prediccion = modelo.predict(nueva_ruta)
+
+print("¿Es buena ruta? (1=Sí, 0=No):", prediccion[0])
