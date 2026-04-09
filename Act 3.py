0446a04abd5a166521bf22e37f9598630b199d1c
Camilo-Arias0 <92058282+Camilo-Arias0@users.noreply.github.com>
2026-04-08 23:29:55 -0400

    Actividad 3

---
+++ Act 3.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# ==============================
+# 1. CREAR DATASET
+# ==============================
+
+data = {
+    "tiempo": [10, 15, 20, 8, 12, 18, 25, 7],
+    "transbordos": [0, 1, 2, 0, 1, 2, 3, 0],
+    "hora_pico": [0, 1, 1, 0, 1, 0, 1, 0],
+    "mejor_ruta": [1, 0, 0, 1, 1, 0, 0, 1]
+}
+
+df = pd.DataFrame(data)
+
